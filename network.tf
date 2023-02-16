c486e876563bc0ed0add0344ff247801b5efa1b1
Jeffrey Medeiros <j.medy90@mail.com>
2023-02-16 20:34:40 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -15,7 +15,7 @@
 module "network" {
   source  = "app.terraform.io/jdm-solutions/network/azurerm"
   version = "3.5.0"
-  resouce_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
