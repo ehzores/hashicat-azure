8ccbe74a0e8f37486511ca0235a202e8ed5cdef0
Jeffrey Medeiros <j.medy90@mail.com>
2023-02-16 20:30:49 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -11,15 +11,11 @@
 #  }
 #}
 
-resource "azurerm_resource_group" "example" {
-  name     = "my-resources"
-  location = "Canada Central"
-}
 
 module "network" {
   source  = "app.terraform.io/jdm-solutions/network/azurerm"
   version = "3.5.0"
-  resouce_group_name = azurerm_resource_group.example.name
+  resouce_group_name = azurerm_resource_group.myresourcegroup.name
   address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   subnet_names        = ["subnet1", "subnet2", "subnet3"]
@@ -36,5 +32,5 @@ module "network" {
     costcenter  = "it"
   }
 
-  depends_on = [azurerm_resource_group.example]
+  depends_on = [azurerm_resource_group.myresourcegroup]
 }
